8b68751d63559c675526bc2b0d17d7264782d2d8
Brian Bycynski <high5bri@hotmail.com>
2020-09-04 01:20:28 -0400

    Init Commit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+//
+//
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyACVOFMedxcMW2POTvDdNrYOAuKvA9DEI8',
+  authDomain: 'clone-cf384.firebaseapp.com',
+  databaseURL: 'https://clone-cf384.firebaseio.com',
+  projectId: 'clone-cf384',
+  storageBucket: 'clone-cf384.appspot.com',
+  messagingSenderId: '603978989584',
+  appId: '1:603978989584:web:a1a99df907a26f34a9ca95',
+};
